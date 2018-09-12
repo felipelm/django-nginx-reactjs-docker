6111bbb3bda07d4adcfb7b44fd23b09f184d4cca
felipe <felipelm123@gmail.com>
2018-09-12 13:53:42 -0300

    connect frontend and backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = ')fn0bm*y%k_@^%k4ofkg01#c-8renvy_uts7lsb@m!z6d9g(8^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = '*'
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  
 ]
 
 ROOT_URLCONF = 'backend.urls'
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const headers = new Headers({
+  Accept: "application/json",
+  "Content-Type": "application/json"
+});
+
 class App extends Component {
+  componentWillMount() {
+    this.testBackend()
+  }
+
+  state = {
+    test: ''
+  }
+
+  testBackend = async () => {
+    const test = await fetch("http://127.0.0.1:8000", {
+      method: "GET",
+      headers: headers,
+      cache: "default"
+    });
+    const testJson = await test.json();
+    if(testJson && testJson.status)
+      this.setState({ test: testJson.status });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          From backend: {this.state.test}
         </p>
       </div>
     );
