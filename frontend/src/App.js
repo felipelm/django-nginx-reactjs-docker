546f0565c95a8bcd2ab3024a40f3b85190f9a2cf
Caley Brock <caleybrock7@gmail.com>
2019-01-02 18:29:19 -0500

    django-admin startproject and create-react-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,25 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-const headers = new Headers({
-  Accept: "application/json",
-  "Content-Type": "application/json"
-});
-
 class App extends Component {
-  componentWillMount() {
-    this.testBackend()
-  }
-
-  state = {
-    test: ''
-  }
-
-  testBackend = async () => {
-    const test = await fetch("http://localhost/test/", {
-      method: "GET",
-      headers: headers,
-      cache: "default"
-    });
-    const testJson = await test.json();
-    if(testJson && testJson.status)
-      this.setState({ test: testJson.status });
-  }
-
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          From backend: {this.state.test}
-        </p>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
