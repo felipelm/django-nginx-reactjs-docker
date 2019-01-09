5aea8469866ef199e8215fe2853d3372fb3d8634
Caley Brock <CBROCK@ad.psc-cfp.gc.ca>
2019-01-09 09:30:03 -0500

    Initialize enzyme testing on frontend

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  shallow(<App />);
 });
