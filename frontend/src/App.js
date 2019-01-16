3e44c225c514790ffbeb20c7bcf4b43e5b2e379b
Francis Normand <francis.normand@canada.ca>
2019-01-16 14:17:04 -0500

    Fixed issues told by ESLint in all .js files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,35 +3,36 @@ import logo from './logo.png';
 import './App.css';
 
 const headers = new Headers({
-  Accept: "application/json",
-  "Content-Type": "application/json"
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
 });
 
 class App extends Component {
   componentWillMount() {
-    this.testBackend()
+    this.testBackend();
   }
 
   state = {
-    test: 'nothing.'
+    test: 'nothing.',
   }
 
   testBackend = async () => {
-    const test = await fetch("http://localhost:80/api/", {
-      method: "GET",
-      headers: headers,
-      cache: "default"
+    const test = await fetch('http://localhost:80/api/', {
+      method: 'GET',
+      headers,
+      cache: 'default',
     });
     const testJson = await test.json();
-    if(testJson && testJson.status)
+    if (testJson && testJson.status) {
       this.setState({ test: testJson.status });
+    }
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             The backend says: {this.state.test}
           </p>
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { shallow } from 'enzyme';
+import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
