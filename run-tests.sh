31033d0f71e2e5aec68aba4d1c923587675bfd62
Michael Cherry <zorackiii@gmail.com>
2019-01-17 14:41:30 -0500

    Added '--watch' command. Execution still hangs on frontend tests, but no longer clears the shell; switched order of tests so that all tests at least complete now

--- run-tests.sh
+++ run-tests.sh
@@ -3,14 +3,14 @@
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 # Build docker (can comment out if already built)
-docker-compose build
-# Test the frontend
-printf "**** ${BLUE}Begin frontend tests${NC} ***\n"
-docker-compose run \
-  --rm frontend npm test
-printf "**** Complete frontend tests ***\n"
+#docker-compose build
 # Test the backened
 printf "**** ${BLUE}Begin backend tests${NC} ***\n"
 docker-compose run \
   --rm backend ./manage.py test
 printf "**** Complete backend tests ***\n"
+# Test the frontend
+printf "**** ${BLUE}Begin frontend tests${NC} ***\n"
+docker-compose run \
+  --rm frontend npm test -- --watch
+printf "**** Complete frontend tests ***\n"
